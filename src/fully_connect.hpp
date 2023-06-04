eed89feca999c91daba756f0487e312a01d0b8e3
wcvanvan <1057736842@qq.com>
2023-06-04 14:57:10 +0800

    update fc

--- src/fully_connect.hpp
+++ src/fully_connect.hpp
@@ -4,14 +4,12 @@
 #include "face_binary_cls.hpp"
 #include "neon_replace.hpp"
 
-bool fully_connect(const Matrix<float>& matrix_in, fc_param param, const Matrix<float>& result_matrix);
+bool fully_connect(const Matrix<float>& matrix_in, const Matrix<float>& result_matrix, fc_param param);
 
-bool fully_connect(const Matrix<float>& matrix_in, fc_param param, const Matrix<float>& result_matrix) {
-	float result[8];
-	fill(result, &result[4], param.p_bias[0]);
-	fill(&result[4], &result[8], param.p_bias[1]);
+bool fully_connect(const Matrix<float>& matrix_in, const Matrix<float>& result_matrix, fc_param param) {
+	auto *result = new float[4 * param.out_features];
 	for (int core_index = 0; core_index < param.out_features; ++core_index) {
-		__m128 sum_vector = _mm_set1_ps(0.0f);
+		__m128 sum_vector = _mm_set1_ps(param.p_bias[core_index]);
 		for (int vector_index = 0; vector_index < param.in_features / 4; ++vector_index) {
 			__m128 vector_matrix_in = _mm_load_ps(&matrix_in.data_start[vector_index * 4]);
 			__m128 vector_weight = _mm_load_ps(&param.p_weight[vector_index * 4 + core_index * param.in_features]);
@@ -20,14 +18,20 @@ bool fully_connect(const Matrix<float>& matrix_in, fc_param param, const Matrix<
 		}
 		_mm_store_ps(&result[core_index * 4], sum_vector);
 	}
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 0; i < 4 * param.out_features; ++i) {
 		result_matrix.data_start[i / 4] += result[i];
 	}
+    delete[] result;
 	// softmax
-	float f1 = exp(result_matrix.data_start[0]);
-	float f2 = exp(result_matrix.data_start[1]);
-	float denominator = f1 + f2;
-	result_matrix.data_start[0] = f1 / denominator;
-	result_matrix.data_start[1] = f2 / denominator;
+    float softmax_sum = 0.0f;
+    auto * fc_result = new float[param.out_features];
+    for (int i = 0; i < param.out_features; ++i) {
+        fc_result[i] = exp(result_matrix.data_start[i]);
+        softmax_sum += fc_result[i];
+    }
+    for (int i = 0; i < param.out_features; ++i) {
+        result_matrix.data_start[i] = fc_result[i] / softmax_sum;
+    }
+    delete[] fc_result;
 	return true;
 }
